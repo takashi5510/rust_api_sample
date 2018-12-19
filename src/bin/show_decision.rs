4d064984e9897211f835b8eb4a510287cea9e3ca
Takashi Ishii <x-ishii@fujisan.co.jp>
2018-12-19 10:48:00 +0900

    mod

--- src/bin/show_decision.rs
+++ src/bin/show_decision.rs
@@ -9,7 +9,7 @@ use self::decision as d;
 fn main() {
  
     let results = d::select()
-        .expect("Error loading t_japan_post_shipment_decision");
+        .expect("Error loading t_japan_post_shipment_decision"); // TODO: expect not use. pattern match switch
 
     println!("Displaying {} t_japan_post_shipment_decision", results.len());
     for decision in results {
